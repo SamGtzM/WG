0d556450d2ff3c74e9f9171ea1d05655897cf426
WOLF <wolf@WOLF.WOLF.NET>
2022-01-11 21:32:16 -0600

    Componente general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { WeareComponent } from './components/weare/weare.component';
-import { ServiceComponent } from './components/service/service.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
     FooterComponent,
-    WeareComponent,
-    ServiceComponent,
-    ContactComponent,
-    ProfileComponent
+    LandingComponent
   ],
   imports: [
     BrowserModule,
