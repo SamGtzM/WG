dd377d8545fa09919e153c073ee303c74a706672
WOLF <wolf@WOLF.WOLF.NET>
2022-01-11 22:09:23 -0600

    Footer delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FooterComponent } from './components/footer/footer.component';
 import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     LandingComponent
   ],
   imports: [
